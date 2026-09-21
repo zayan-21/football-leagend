005cfad69ee625b8ae1fe0728484926cd0032917
zayan <ask2zayan@gmail.com>
2026-09-21 15:52:21 +0500

    Replace grading system with user input prompts
    Removed grading logic and replaced it with prompts for user email and password.

--- football.js
+++ football.js
@@ -1,19 +1,7 @@
+var userName = prompt("Type your email");
 
-var marks = prompt("give us marks");
+console.log(userName);
 
+var password = prompt("Type your password");
 
-if(marks >= 90){
-    console.log("Grade A");
-    
-}else if(marks >= 80){
-    console.log("Grade B");
-    
-}else if(marks >= 70){
-    console.log("Grade C");
-    
-}else if(marks >= 60){
-    console.log("Grade D");
-}else{
-    console.log("You Failed!");
-    
-}
+console.log(password);
